e03589b23a2ed54dc32da5515d66cbb5406a29be
John Comeau <jc@unternet.net>
2025-10-18 16:08:29 +0000

    better dumpraw shows bogus fontPtr

--- src/tk8.6.10/unix/tkUnixRFont.c
+++ src/tk8.6.10/unix/tkUnixRFont.c
@@ -69,8 +69,10 @@ void dumpraw(const char *header, unsigned char *buffer, size_t bufsize);
 
 void dumpraw(const char *header, unsigned char *buffer, size_t bufsize) {
     fprintf(stderr, "%s (0x%p): ", header, buffer);
-    for (int i = 0; i < bufsize; i++) {
-        fprintf(stderr, "%02x", (unsigned char)buffer[i]);
+    for (int i = 0, j = 0; i < bufsize; i++) {
+        j = buffer[i];
+	if (' ' <= j && j <= '~') fputc(buffer[i], stderr);
+        else fprintf(stderr, "\\x%02x", (unsigned char)j);
     }
     fprintf(stderr, "\n");
 }
